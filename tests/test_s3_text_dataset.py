c5eddb723a60374084c47ff7c0303e4658b8a9a7
Steve Madere <steve@stevemadere.com>
2023-12-31 10:16:05 -0600

    Changed prefix (inserted leading /text/) for test data in vast4elephant bucket

--- tests/test_s3_text_dataset.py
+++ tests/test_s3_text_dataset.py
@@ -7,6 +7,6 @@ from datasets import Dataset
 from s3datasets import S3TextDataset
 
 def test_from_bucket() -> None:
-    ds: Dataset = S3TextDataset.from_bucket("vast4elephant", "arXiv/CL/")
+    ds: Dataset = S3TextDataset.from_bucket("vast4elephant", "text/arXiv/CL/")
     assert ds
     assert re.search('Abstract\n',ds[1]["text"])
